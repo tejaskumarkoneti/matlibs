b290182cd701d89224ef5e2382154014f0450771
Tejaskumar <66914227+tejaskumarkoneti@users.noreply.github.com>
2024-01-18 17:21:35 +0530

    Add files via upload

---
+++ tiger.py
@@ -0,0 +1,17 @@
+
+Adjective1 = input("Adjective1")
+Adjective2 = input("Adjective2")
+Adjective3 = input("Adjective3")
+Number = int(input("Number"))
+Animal = input("Animal")
+Place = input("Place")
+Verb = input("Verb")
+Adverb = input("Adverb")
+Color = input("Color")
+
+
+
+print(f"""In the {Adjective1} world of wild life, where {Number} majestic {Animal}s roam freely,
+       there is a {Place} that showcases the {Adjective2} diversity of nature. 
+       Visitors can {Verb} {Adverb} through lush landscapes and witness the {Color} beauty of the surroundings in bus. 
+      It's a {Adjective3} experience that captures the essence of the animal kingdom.""")
